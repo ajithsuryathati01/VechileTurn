1dc8db1ef8ebb0215baaca7471f0e49009f2188e
ajithsuryathati01 <ajithsuryathati01@gmail.com>
2021-12-11 13:53:38 +0530

    final values

---
+++ app/src/main/java/uk/ac/tees/aad/W9506463/allServices.java
@@ -0,0 +1,47 @@
+package uk.ac.tees.aad.W9506463;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+public class allServices extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_services);
+
+        LinearLayout gs  = findViewById(R.id.gs);
+        LinearLayout vec  = findViewById(R.id.vechileWash);
+        LinearLayout spare = findViewById(R.id.spare);
+
+        gs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent  = new Intent(getApplicationContext(),Details.class);
+                intent.putExtra("option","service");
+                startActivity(intent);
+
+            }
+        });
+        vec.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent  = new Intent(getApplicationContext(),Details.class);
+                intent.putExtra("option","clean");
+                startActivity(intent);
+            }
+        });
+        spare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent  = new Intent(getApplicationContext(),Details.class);
+                intent.putExtra("option","spare");
+                startActivity(intent);
+            }
+        });
+    }
+}
